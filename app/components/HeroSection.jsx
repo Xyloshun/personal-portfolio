8e27ede5051b5e6f26f435b71584aa2986c36c52
Xyloshun <xyloshun@gmail.com>
2024-05-06 16:43:00 -0400

    pretty colors

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,23 +4,26 @@ import Image from 'next/image';
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-            Hello, I'm Leonard
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white max-w-2xl mb-4 sm:text-5xl lg:text-6xl text-4xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">Hello, I'm {" "}</span>
+            Leonard
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             If I applied to a job and this is the site that appears, please click the download button for my updated CV for more accurate links. My C++ project portfolio PDF can be downloaded with the second button as well. This is a very early release of my portfolio to accomodate any older versions of my CV I've sent. Thank you!
           </p>
         <div>
           <a href="/downloads/nguyenleonard-comp4.pdf">
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 text-black mr-4">
               Projects PDF
             </button>
           </a>
           <a href="/downloads/nguyenleonard-CV.pdf" download="nguyenleonard-CV">
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-black-500 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </a>
         </div>
